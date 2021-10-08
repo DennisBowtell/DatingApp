bdd6e2e034ac1d416ee87c3a5e18fe6b549b6083
Dennis Bowtell <dennis@baxterit.com.au>
2021-10-09 10:48:10 +1100

    Dropdown

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-
-  constructor() { }
+  model: any = {}
+  loggedIn: boolean = false;
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response);
+      this.loggedIn = true;
+    }, error => {
+      console.log(error);
+      this.loggedIn = false;
+    })
+  }
+
+  logout() {
+    this.loggedIn = false;
+    //this.model = null;
   }
 
 }
