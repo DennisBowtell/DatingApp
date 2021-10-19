679d2ee5c994757c6c367b12ae7baad3cbe338e9
Dennis Bowtell <dennis@baxterit.com.au>
2021-10-20 07:48:47 +1100

    End of section 8

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,15 +1,44 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Extentions;
 
 namespace API.Entities
 {
     public class AppUser
     {
         public int Id { get; set; }
-        public String UserName { get; set; }
+        [MaxLength(100)]
+        public string UserName { get; set; }
+        [MaxLength(200)]
         public byte[] PasswordHash { get; set; }
+        [MaxLength(200)]
         public byte[] PasswordSalt { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        [MaxLength(50)]
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        [MaxLength(20)]
+        public string Gender { get; set; }
+        [MaxLength(2000)]
+        public string Introduction { get; set; }
+        [MaxLength(2000)]
+        public string LookingFor { get; set; }
+        [MaxLength(2000)]
+        public string Interests { get; set; }
+        [MaxLength(50)]
+        public string City { get; set; }
+        [MaxLength(50)]
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+
+        // public int GetAge()
+        // {
+        //     return  DateOfBirth.CalculateAge();
+        // }
+
     }
 }
