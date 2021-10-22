d0907105fc77bd1811c0fd3a04c2cd51535899c8
Dennis Bowtell <dennis@baxterit.com.au>
2021-10-22 11:12:45 +1100

    End of Section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Authorization;
 using API.Interfaces;
 using API.DTOs;
 using AutoMapper;
+using System.Security.Claims;
 
 namespace API.Controllers
 {
@@ -41,11 +42,26 @@ namespace API.Controllers
             return await _userRepository.GetMemberByUsernameAsync(username);
         }
 
-        // GET: api/Users/5
+        // GET: api/Users/id/5
         [HttpGet("id/{id}")]
         public async Task<ActionResult<MemberDto>> GetUserById(int id)
         {
             return await _userRepository.GetMemberByIdAsync(id);
+        }
+        
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+            
+            _mapper.Map(memberUpdateDto, user);
+            
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
         }
 
     }
